9a6a6becda08023f85526b0dfcaa8c92a607ed7c
unknown <malkindi.storm@gmail.com>
2026-03-30 00:32:46 -0700

    updated to funtction of ip validator

--- Day1/ip_validator.py
+++ Day1/ip_validator.py
@@ -1,26 +1,35 @@
+"""This function is an IP validator that checks if the entered IP address is a valid IPv4 address."""
+
+def is_valid_ipv4(ipv4):
+    # Split the IPv4 address into 4 parts
+    ipsplit = ipv4.split(".")
+    
+    # Check if the length of the split parts is 4
+    if len(ipsplit) != 4:
+        return False
+    
+    for ip in ipsplit:
+        # Check if each part is a digit
+        if not ip.isdigit():
+            return False
+        
+        number = int(ip)
+        
+        # Check if each part is between 0 and 255
+        if number < 0 or number > 255:
+            return False
+        
+        # Check if the string representation of the number matches the original part (to avoid leading zeros)
+        if str(number) != ip:
+            return False
+            
+    return True
 #Prompt the user to Enter A valid IPv4 Address
 ipv4=(input("enter an ipv4 Address: "))
-#Spliting the IPv4 entered into 4 parts
-ipsplit=ipv4.split(".")
-#Condition for checking if the ip is valid 
-condition=True
-#If Statement for checking the lenth of the entered ip
-if not (len(ipsplit)==4):
-    condition=False
+is_valid = is_valid_ipv4(ipv4)
 
-# for loop to check each number
-for ip in ipsplit:
-    #If statement to check if ach number is a digit number
-    if not ip.isdigit():
-        condition=False
-    number=int(ip)
-    #If Statement to check if the number enetered for each part is between 0 and 255
-    if number<0 or number>255:
-        condition=False
-    if number==str(ip):
-        condition=False
 #print to the user that the ip is valid
-if (condition==True):
+if is_valid:
     print("Your IPv4 is valid")
 #print to the user that the ip is not valid
 else:
